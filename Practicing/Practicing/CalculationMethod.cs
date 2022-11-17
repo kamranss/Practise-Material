b4d6b7b6c06e910826314c9d8370b5c040016ee3
kamranss <kamranss@code.edu.az>
2022-11-17 16:53:03 +0400

    feature: added new Project Calculator

---
+++ Practicing/Practicing/CalculationMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing
+{
+    internal class CalculationMethod
+    {
+
+        public CalculationMethod()
+        {
+
+        }
+
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public double Product(double a, double b)
+        {
+            return a * b;
+        }
+        public double Deduction(double a, double b)
+        {
+            return a - b;
+        }
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Practicing/Practicing/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Practicing;
+using System;
+using System.Text;
+
+
+#region Make Password unvisable
+////Make password unvisable
+//Console.Write("You password please: ");
+//StringBuilder passwordBuilder = new StringBuilder();
+//bool continueReading = true;
+//char newLineChar = '\r';
+
+
+//while (continueReading)
+//{
+//    ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+//    char passwordChar = consoleKeyInfo.KeyChar;
+
+//    if (passwordChar == newLineChar)
+//    {
+//        continueReading = false;
+//    }
+//    else
+//    {
+//        Console.Write("*");
+//        passwordBuilder.Append(passwordChar.ToString());
+//    }
+//}
+//Console.WriteLine();
+//Console.Write("Your password in plain text is {0}", passwordBuilder.ToString());
+#endregion
+
+Calculator Calculation1 = new Calculator();
+
+Console.WriteLine(" Write First number ->>");
+Calculation1.Num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Write Second number ->>");
+Calculation1.Num2 = double.Parse(Console.ReadLine());
+Console.WriteLine(" choose one of the following methods");
+Console.WriteLine(" for  +  ->>press->> 1");
+Console.WriteLine(" for  *  ->>press->> 2");
+Console.WriteLine(" for  -  ->>press->> 3");
+Console.WriteLine(" for  /  ->>press->> 3");
+
+Calculation1.Variation = Console.ReadLine();
+Console.WriteLine($"Result =" + Calculation1.CalculatorMethod(Calculation1.Num1, Calculation1.Num2, Calculation1.Variation));
+        
+
+
