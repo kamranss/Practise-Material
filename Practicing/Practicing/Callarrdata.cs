dd4eade47fe70dc443b959037e54de26df7f3bd7
kamranss <kamranss@code.edu.az>
2022-11-20 23:36:37 +0400

    Refactor: updated Practise material

---
+++ Practicing/Practicing/Callarrdata.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing
+{
+    internal class Callarrdata
+    {
+
+
+        public Callarrdata()
+        {
+
+        }
+
+        public string ArrayOnScreen(string[] a)
+        {
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            return "finished";
+
+        }
+
+
+        public void CalculateArr(int[,] a)
+        {
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                sum1 += a[i,i];
+            }
+            int j = a.GetLength(0) - 1;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                sum2 += a[i,j];
+                j--;
+            }
+            Console.WriteLine( sum1-sum2 );
+
+
+
+
+        }
+    }
+
+}
+
+
+
+
+   
+
---
+++ Practicing/Practicing/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Practicing;
+using System;
+using System.Text;
+
+
+#region Make Password unvisable
+////Make password unvisable
+//Console.Write("You password please: ");
+//StringBuilder passwordBuilder = new StringBuilder();
+//bool continueReading = true;
+//char newLineChar = '\r';
+
+
+//while (continueReading)
+//{
+//    ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+//    char passwordChar = consoleKeyInfo.KeyChar;
+
+//    if (passwordChar == newLineChar)
+//    {
+//        continueReading = false;
+//    }
+//    else
+//    {
+//        Console.Write("*");
+//        passwordBuilder.Append(passwordChar.ToString());
+//    }
+//}
+//Console.WriteLine();
+//Console.Write("Your password in plain text is {0}", passwordBuilder.ToString());
+#endregion
+
+#region Calculator
+//Calculator Calculation1 = new Calculator();
+
+//Console.WriteLine(" Write First number ->>");
+//Calculation1.Num1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Write Second number ->>");
+//Calculation1.Num2 = double.Parse(Console.ReadLine());
+//Console.WriteLine(" choose one of the following methods");
+//Console.WriteLine(" for  +  ->>press->> 1");
+//Console.WriteLine(" for  *  ->>press->> 2");
+//Console.WriteLine(" for  -  ->>press->> 3");
+//Console.WriteLine(" for  /  ->>press->> 3");
+
+//Calculation1.Variation = Console.ReadLine();
+//Console.WriteLine($"Result = " + Calculation1.CalculatorMethod(Calculation1.Num1, Calculation1.Num2, Calculation1.Variation));
+#endregion    
+
+#region Number category
+
+//Numbercategory step1 = new Numbercategory();
+
+//Console.WriteLine("Enter the number");
+
+//step1.Num = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(step1.NumberCategory(step1.Num));
+
+
+#endregion
+
+#region Lab work calling method within class
+//Callarrdata Firstattempt1 = new Callarrdata();
+
+//string[] array = { "salam", "necese" };
+
+//Firstattempt1.ArrayOnScreen(array);
+
+#endregion
+
+#region Sum arr diognals and than implement deduction
+//int  [,] arr = { {1, 2, 3}, {4, 5, 6},{7, 8, 9}};
+
+//Callarrdata Callarrdata1 = new Callarrdata();
+
+//Callarrdata1.CalculateArr(arr);
+#endregion
+
+#region Method for spliting string to parts and assigning to new Arr
+//Arrmethods method1 = new Arrmethods();
+//Console.WriteLine("Write some word or numbers");
+//string a = Console.ReadLine();
+
+//method1.Arr1 = a.Split(",");
+
+//method1.ArrMethod1(method1.Arr1);
+#endregion
+
